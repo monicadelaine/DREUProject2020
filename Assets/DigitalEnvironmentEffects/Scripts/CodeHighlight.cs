789ea0918cb19b4ec0a148f8fab9083ae9cc075b
kmwright1 <kmwright1@aggies.ncat.edu>
2020-08-06 12:08:58 -0400

    Code step through

---
+++ Assets/DigitalEnvironmentEffects/Scripts/CodeHighlight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeHighlight : MonoBehaviour
+{
+    public GameObject line1;
+    public GameObject line2;
+
+    private MeshRenderer ren1;
+    private MeshRenderer ren2;
+  void OnTriggerEnter(Collider button)
+    {
+        if (button.CompareTag("Player"))
+        {
+           
+            ren1 = line1.GetComponent<MeshRenderer>();
+            ren2 = line2.GetComponent<MeshRenderer>();
+
+            ren1.enabled = !ren1.enabled;
+            ren2.enabled = !ren2.enabled;
+        }
+    }
+ 
+}
---
+++ Assets/codestep6.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class codestep6 : MonoBehaviour
+{
+    public GameObject line6;
+    public GameObject line7;
+
+    private MeshRenderer ren6;
+    private MeshRenderer ren7;
+
+    void OnTriggerEnter(Collider button)
+    {
+        if (button.CompareTag("Player"))
+        {
+            ren6 = line6.GetComponent<MeshRenderer>();
+            ren7 = line7.GetComponent<MeshRenderer>();
+
+            ren6.enabled = !ren6.enabled;
+            ren7.enabled = !ren7.enabled;
+        }
+    }
+}
